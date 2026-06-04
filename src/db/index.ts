ca099e22bb40192c526471d98c38ae0c69c2f8b3
Occult09 <maheermusfiq221@gmail.com>
2026-06-04 17:19:42 +0600

    created db tables for users and issues

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,7 +6,33 @@ export const pool = new Pool({
 })
 
 export const initDB = async () => {
-    await pool.query(`
-        
-        `)
+    try {
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name TEXT,
+            email TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role VARCHAR(20) DEFAULT 'contributer',
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS issues(
+                id SERIAL PRIMARY KEY,
+                title text,
+                description text,
+                type text,
+                status VARCHAR(20) DEFAULT 'open',
+                reporter_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+            )
+            `)
+        console.log("Database Connected Successfully!");
+    } catch (error) {
+        console.log(error);
+    }
 }
